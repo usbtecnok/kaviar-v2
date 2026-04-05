f756bd98ea9dc640033775412e906080adff6c83
Aparecido Goes <aparecido.goes2018@gmail.com>
2026-04-05 15:55:30 -0300

    chore(site): update download link to passageiro v12 (fix ergonomia emergência)

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -364,7 +364,7 @@ function Home() {
               <Button
                 variant="contained"
                 fullWidth
-                href="https://downloads.kaviar.com.br/kaviar-passageiro-v11.apk"
+                href="https://downloads.kaviar.com.br/kaviar-passageiro-v12.apk"
                 target="_blank"
                 rel="noopener noreferrer"
                 sx={{
