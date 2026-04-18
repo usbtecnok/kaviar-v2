488c8cfbe5d72bc44ea1ec4899e7a14ba81b2a24
Aparecido Goes <aparecido.goes2018@gmail.com>
2026-04-18 19:43:05 -0300

    chore: bump versão motorista para 1.7.1 (safe area + touch target)

--- app.config.js
+++ app.config.js
@@ -48,7 +48,7 @@ export default {
     owner: 'usbtecnok',
     name: variantConfig.name,
     slug: variantConfig.slug,
-    version: '1.4.0',
+    version: '1.7.1',
     orientation: 'portrait',
     icon: variantConfig.icon,
     userInterfaceStyle: 'light',
