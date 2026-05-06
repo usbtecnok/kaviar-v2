c60bf9ce6248b0af778983fcb508de70d8b91a58
Aparecido Goes <aparecido.goes2018@gmail.com>
2026-05-06 19:13:47 -0300

    chore: bump version to 1.11.9 for driver KAVIAR sound APK

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
     owner: 'usbtecnok',
     name: variantConfig.name,
     slug: variantConfig.slug,
-    version: '1.11.8',
+    version: '1.11.9',
     orientation: 'portrait',
     icon: variantConfig.icon,
     userInterfaceStyle: 'light',
