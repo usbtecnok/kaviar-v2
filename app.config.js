cac6c9782fad745d1189db17c7853b6e6609cce6
Aparecido Goes <aparecido.goes2018@gmail.com>
2026-05-06 10:58:50 -0300

    chore: bump version to 1.11.6 for driver accept+alert fix APK

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
     owner: 'usbtecnok',
     name: variantConfig.name,
     slug: variantConfig.slug,
-    version: '1.11.5',
+    version: '1.11.6',
     orientation: 'portrait',
     icon: variantConfig.icon,
     userInterfaceStyle: 'light',
