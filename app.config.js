d0b1600f14eb4182db35b6bed1aad0e7fbc2281e
Aparecido Goes <aparecido.goes2018@gmail.com>
2026-05-06 21:08:09 -0300

    chore: bump version to 1.11.10 for KAVIAR sound channel v2 APK

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
     owner: 'usbtecnok',
     name: variantConfig.name,
     slug: variantConfig.slug,
-    version: '1.11.9',
+    version: '1.11.10',
     orientation: 'portrait',
     icon: variantConfig.icon,
     userInterfaceStyle: 'light',
