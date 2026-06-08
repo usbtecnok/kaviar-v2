65f447bac191732f8565775e4d71afbe80577c50
Aparecido Goes <aparecido.goes@gmail.com>
2026-06-08 20:41:44 -0300

    chore(mobile): bump passageiro para v1.13.6-women-ux-final

--- app.config.js
+++ app.config.js
@@ -22,8 +22,8 @@ const config = {
     adaptiveIcon: './assets/adaptive-icon-passenger.png',
     scheme: 'kaviar-passenger',
     projectId: '23cab91b-82a5-4d92-9709-017279a2539d',
-    version: '1.13.5-modal-focus-fix',
-    versionCode: 19,
+    version: '1.13.6-women-ux-final',
+    versionCode: 20,
   }
 };
 
