fb64dc9a3414a656b57297fb6edb4f2a631e834e
Aparecido Goes <aparecido.goes@gmail.com>
2026-06-08 18:53:26 -0300

    chore(mobile): bump passageiro para v1.13.4-women-preference-invite

--- app.config.js
+++ app.config.js
@@ -22,8 +22,8 @@ const config = {
     adaptiveIcon: './assets/adaptive-icon-passenger.png',
     scheme: 'kaviar-passenger',
     projectId: '23cab91b-82a5-4d92-9709-017279a2539d',
-    version: '1.13.3-women-preference-copy',
-    versionCode: 17,
+    version: '1.13.4-women-preference-invite',
+    versionCode: 18,
   }
 };
 
